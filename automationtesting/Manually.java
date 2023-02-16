e56723c74e3034e6ef7c7602902688f0f2050631
MonaPatel1223 <minalthakor1223@gmail.com>
2023-02-16 22:19:30 +0000

    java

--- automationtesting/Manually.java
+++ automationtesting/Manually.java
@@ -1,2 +1,8 @@
-package automationtesting;public class Manually {
+package automationtesting;
+
+public class Manually {
+    public static void main(String[] args)
+    {
+        System.out.println("Testing Levels");
+    }
 }
