2a37f2e637f20db8857e1157935528ba7fa565bf
MonaPatel1223 <minalthakor1223@gmail.com>
2023-02-16 21:59:36 +0000

    practice

---
+++ automationtesting/AutomationTopic.java
@@ -0,0 +1,12 @@
+package automationtesting;
+
+public class AutomationTopic
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Automation testing is faster than manual");
+        System.out.println("Required Knowledge of Programming language");
+
+        /* here I Write the Advantages of Automation Tesing*/
+    }
+}
---
+++ manualtesting/ManualTopic.java
@@ -0,0 +1,10 @@
+package manualtesting;
+// this is package name
+
+public class ManualTopic {
+    public static void main(String[] args)
+    {
+        System.out.println("Software Types");
+        System.out.println("software testing models");
+    }
+}
